d078a9c6e0f47b19b593ea03c9656dffb80b82fc
b1ron <goldingbyron@gmail.com>
2024-10-02 19:45:54 +0200

    comments

--- server.go
+++ server.go
@@ -1,26 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"jwt/jwt"
 	"log"
 	"net/http"
 	"os"
 )
 
-// server is a simple HTTP server that uses JWT for authentication.
+// server is a simple HTTP server that uses JWT for authentication. It also acts as a session manager.
 // It has two endpoints:
-// - /login: expects a POST request with a JSON body containing a "username" key.
+// - /login: expects a POST request with a JSON body containing a "username" and password.
+//  If the credentials are valid, it responds with a JWT token. The secret used to sign the token. A refresh token is set as a cookie.
 // - /resource: expects a GET request with a "Authorization" header containing a JWT token.
-
-var resources = map[string]string{
-	"1234567890": "some resource",
-	"0987654321": "another resource",
-	"1122334455": "yet another resource",
-	"5432167890": "one more resource",
-	"1010101010": "cannot access this resource",
-}
+// If the token is valid, it responds with a JSON object containing the requested resource.
 
 func main() {
+	token, err := jwt.Encode(map[string]interface{}{
+		"iat":  1516239022,
+		"name": "John Doe",
+	}, "secret", "HS256")
+	if err != nil {
+		log.Fatalf("could not encode token: %v", err)
+	}
+	_ = token
 	secret, err := os.ReadFile("secret.txt")
 	if err != nil {
 		log.Fatalf("could not read secret: %v", err)
@@ -33,10 +35,8 @@ func main() {
 
 func login(w http.ResponseWriter, r *http.Request) {
 	// TODO ...
-	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
 
 func resource(w http.ResponseWriter, r *http.Request) {
 	// TODO ...
-	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
