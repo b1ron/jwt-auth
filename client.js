cc4324c1eef6a3c09f3837dcf3065cface3679da
b1ron <goldingbyron@gmail.com>
2024-10-06 00:11:58 +0200

    correctly await login

--- client.js
+++ client.js
@@ -5,30 +5,34 @@ formData.append('username', 'John Doe');
 
 let token = '';
 
-const login = fetch('http://localhost:8000/login', {
-    method: 'POST',
-    credentials: 'include',
-    body: formData,
-}).then(function(resp) {
-    resp.headers.getSetCookie().forEach(function(cookie) {
-        if (cookie.startsWith('refreshToken')) {
-            token = cookie.split('=')[1];
-            return;
-        };
+const login = async function() {
+    await fetch('http://localhost:8000/login', {
+        method: 'POST',
+        credentials: 'include',
+        body: formData,
+    }).then(function(resp) {
+        resp.headers.get('Set-Cookie').split(';').forEach(function(cookie) {
+            if (cookie.startsWith('refreshToken')) {
+                token = cookie.split('=')[1];
+                return;
+            };
+        });
     });
-});
+}
 
+const resource = async function() {
+    await fetch('http://localhost:8000/resource', {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + token,
+        },
+    }).then(function(resp) {
+        return resp.text();
+    });
+};
 
-const resource = fetch('http://localhost:8000/resource', {
-    method: 'GET',
-    headers: {
-        'Authorization': 'Bearer ' + token,
-    },
-}).then(function(resp) {
-    return resp.text();
-});
-
-(async () => {
-    await login;
-    console.log(token);
-  })();
+login().then(function() {
+    resource().then(function(data) {
+        console.log(data);
+    });
+})
