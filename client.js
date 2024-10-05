c55e44280750f725004943e9c8aa7552eae5bcd7
b1ron <goldingbyron@gmail.com>
2024-10-05 11:29:37 +0200

    store token as global variable

--- client.js
+++ client.js
@@ -5,9 +5,19 @@ formData.append('username', 'John Doe');
 
 let token = '';
 
-fetch('http://localhost:8000/login', {
+const login = fetch('http://localhost:8000/login', {
     method: 'POST',
     body: formData,
 }).then(function(resp) {
-    resp.headers.forEach(function(val, key) { console.log(key + ' -> ' + val); });
+    resp.headers.forEach(function(val, key) { 
+        if (key === 'authorization') {
+            token = val;
+            return token;
+        };
+    });
 });
+
+(async () => {
+    await login;
+    console.log(token);
+  })();
