f96b4a485fdb75db30c192784a336636defe1e20
b1ron <goldingbyron@gmail.com>
2024-10-06 10:47:32 +0200

    contentType should be x-www-form-urlencoded

--- client.js
+++ client.js
@@ -1,7 +1,8 @@
 // a simple JavaScript client to test the endpoints
 
 let formData = new FormData();
-formData.append('username', 'John Doe');
+formData.set('username', 'John Doe');
+formData.set('password', 'password');
 
 let token = '';
 
@@ -9,6 +10,7 @@ const login = async function() {
     await fetch('http://localhost:8000/login', {
         method: 'POST',
         credentials: 'include',
+        contentType: 'application/x-www-form-urlencoded',
         body: formData,
     }).then(function(resp) {
         resp.headers.get('Set-Cookie').split(';').forEach(function(cookie) {
@@ -27,14 +29,13 @@ const resource = async function() {
             'Authorization': 'Bearer ' + token,
         },
     }).then(function(resp) {
-        // FIXME: 401 Unauthorized
         console.log(resp.status, resp.statusText);
-        return resp.text();
+        return resp;
     });
 };
 
 login().then(function() {
     resource().then(function(data) {
-        console.log(token, data);
+        console.log(token, data); // FIXME: data is undefined
     });
 })
