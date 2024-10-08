858dd300c1b784abc5b47c082d6cbc0f7ade3473
b1ron <goldingbyron@gmail.com>
2024-10-08 14:55:06 +0200

    avoid init and global session store

--- server.go
+++ server.go
@@ -25,42 +25,45 @@ type session struct {
 }
 
 // store is a simple in-memory session store where the key is the username and the value is a session.
-var store = make(map[string]*session)
+type store struct {
+	sessions map[string]*session
+}
 
-func init() {
+func main() {
+	// make the session store for the handlers
+	store := &store{
+		sessions: make(map[string]*session),
+	}
 	f, err := os.ReadFile("secret.txt")
 	if err != nil {
 		log.Fatalf("could not read secret: %v", err)
 	}
 	secret := strings.Trim(string(f), "\n")
-	store["init"] = &session{
+	store.sessions["init"] = &session{
 		secret: secret,
 	}
-}
-
-func main() {
-	http.HandleFunc("/login", login)
-	http.HandleFunc("/resource", resource)
+	http.HandleFunc("/login", store.login)
+	http.HandleFunc("/resource", store.resource)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
-func login(w http.ResponseWriter, r *http.Request) {
+func (s *store) login(w http.ResponseWriter, r *http.Request) {
 	token, err := jwt.Encode(map[string]interface{}{
 		"iat":  time.Now().Unix(),
 		"name": r.FormValue("username"),
-	}, store["init"].secret, "HS256")
+	}, s.sessions["init"].secret, "HS256")
 	if err != nil {
 		fmt.Fprintf(w, "could not encode token: %v", err)
 	}
-	store[r.FormValue("username")] = &session{
+	s.sessions[r.FormValue("username")] = &session{
 		token:  token,
-		secret: store["init"].secret,
+		secret: s.sessions["init"].secret,
 	}
 	http.SetCookie(w, &http.Cookie{Name: "refreshToken", Value: token})
 	w.WriteHeader(http.StatusOK)
 }
 
-func resource(w http.ResponseWriter, r *http.Request) {
+func (s *store) resource(w http.ResponseWriter, r *http.Request) {
 	token := r.Header.Get("Authorization")
 	if token == "" {
 		http.Error(w, "missing token", http.StatusUnauthorized)
@@ -74,7 +77,7 @@ func resource(w http.ResponseWriter, r *http.Request) {
 	}
 	claimsM := claims.Map()
 	username := claimsM["name"].(string)
-	if err := jwt.Validate(token, store[username].secret); err != nil {
+	if err := jwt.Validate(token, s.sessions[username].secret); err != nil {
 		http.Error(w, err.Error(), http.StatusUnauthorized)
 		return
 	}
